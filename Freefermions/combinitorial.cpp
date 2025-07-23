f457b847029ecaf33e537cf7fedfeedb3136a403
Gonzalo Alvarez <gz1@ornl.gov>
2025-07-23 19:57:38 -0400

    combinatorial: fixed fermion sign

--- Freefermions/combinitorial.cpp
+++ Freefermions/combinitorial.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <iomanip>
+#include <cassert>
 #include "LanczosPlusPlus/src/Models/HubbardOneOrbital/BasisOneSpin.h"
-#include "../codes/PsimagLite/src/Matrix.h"
+#include "PsimagLite/src/Matrix.h"
+#include "PsimagLite/src/BitManip.h"
 
 PsimagLite::Matrix<SizeType> LanczosPlusPlus::BasisOneSpin::comb_;
 SizeType LanczosPlusPlus::BasisOneSpin::nsite_;
@@ -33,6 +35,43 @@ double computeCorrelation(unsigned int site, const std::vector<double>& gs, cons
 	return sum;
 }
 
+void symmetrize(PsimagLite::Matrix<double>& matrix)
+{
+	unsigned int rank = matrix.rows();
+	assert(rank == matrix.cols()); // matrix must be square
+	for (unsigned int row = 0; row < rank; ++row) {
+		for (unsigned int col = row + 1; col < rank; ++col) {
+			matrix(col, row) = matrix(row, col);
+		}
+	}
+}
+
+// Sign of c^\dagger_ind c_jnd applied to state |state>
+int fermionSign(unsigned int state, unsigned int ind, unsigned int jnd, int fermion_sign)
+{
+	if (ind == jnd) {
+		err(std::to_string(ind) + " must be different than " + std::to_string(jnd) + "\n");
+	}
+
+
+	unsigned int maskindless = (1 << ind) - 1;
+	unsigned int maskjndless = (1 << jnd) - 1;
+
+	unsigned int tmpindless = (maskindless & state);
+	unsigned int tmpjndless = (maskjndless & state);
+	unsigned int count = PsimagLite::BitManip::countKernighan(tmpindless) + PsimagLite::BitManip::countKernighan(tmpjndless);
+	if (state == 3) std::cout<<"fs===="<<state<<"    "<<ind<<" "<<jnd<<"    "<<count<<"\n";
+
+	if (ind > jnd) {
+		++count;
+	}
+
+	int val = (count & 1) ? fermion_sign : 1;
+
+	std::cout<<"                   count="<<count<<"   val="<<val<<"\n";
+	return val;
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc != 4) {
@@ -41,6 +80,7 @@ int main(int argc, char* argv[])
 	}
 
 	bool periodic = true;
+	int FERMION_SIGN = -1; // set to -1 for fermions, 1 for bosons
 	// Number of sites
 	unsigned int n = atoi(argv[1]); // don't use atoi atof std:: something to double need to search on the web
 	unsigned int npart = atoi(argv[2]);
@@ -64,12 +104,12 @@ int main(int argc, char* argv[])
 	LanczosPlusPlus::BasisOneSpin bos(n, npart);
 	unsigned int hilbert = bos.size();
 	std::cout<< "hilbert " << hilbert<<"\n";	
- 	PsimagLite::Matrix<double> hamiltonian(hilbert, hilbert);	
+	PsimagLite::Matrix<double> hamiltonian(hilbert, hilbert);	
 
 	// More intuitive way maybe:
 	// loop over row and nested loop over col <col|H|row> = complex number = H(row, col)
 	//
-	
+
 	for (unsigned int row = 0; row < hilbert; ++row) {
 		// Every integer from 0 to 2^n - 1 represents a state of the (computational) basis
 		// Example Alice looks like 0 1 1 --> interpret it as a binary number --> 3
@@ -81,7 +121,7 @@ int main(int argc, char* argv[])
 		std::cout<<"statei"<<statei<<"\n";
 
 		for (unsigned int site = 0; site < n; ++site) {
-			
+
 			// Starting with state row 
 			// flip bit number site and bit number site + 1
 			// We need to worry about border, like if site + 1 >= n
@@ -95,9 +135,9 @@ int main(int argc, char* argv[])
 				next_site = 0;
 			}*/
 			// "ternary operator in C and C++"
-			
+
 			bool isborder = ((site + 1) == n);
-			
+
 			if (isborder and !periodic) {
 				continue; // or break
 			}
@@ -105,19 +145,30 @@ int main(int argc, char* argv[])
 			unsigned int next_site = (isborder) ? 0 : site + 1;
 			unsigned int masksite = (1<<site);
 			unsigned int masknext = (1<<next_site);
-			unsigned int resultsite = statei & masksite;
-			unsigned int resultnext = statei & masknext;
-			if (resultsite == 0 && resultnext == 0) continue;
-			if (resultsite > 0 && resultnext > 0) continue;
+			unsigned int resultsite = (statei & masksite) ? 1 : 0;
+			unsigned int resultnext = (statei & masknext) ? 1 : 0;
+
+			if (resultsite == resultnext) continue;
 
 			unsigned int mask = (1<<site) | (1<<next_site); // The "|" or binary op
 			unsigned int statej = statei ^ mask; // ^ is the xor binary op
 			unsigned int col = bos.perfectIndex(statej);
-			//std::cout<<"if I apply cdagger_"<<site<<" c_"<<next_site<<" to state ";
-			//std::cout<<statei<<" ("<<row<<") I get state "<<statej<<" which has index "<<col<<"\n";
-			hamiltonian(row, col) = couplingJ;
+
+			int fs = 1;
+			if (resultsite == 1 && resultnext == 0) {
+				// destroy at site and create at next
+				fs = fermionSign(statei, next_site, site, FERMION_SIGN);
+			} else {
+				fs = fermionSign(statei, site, next_site, FERMION_SIGN);
+			}
+
+
+			std::cout<<row<<" "<<col<<"      "<<fs<<"\n";
+			hamiltonian(row, col) = couplingJ * fs;
 		}
 	}
+
+	//symmetrize(hamiltonian);
 
 	std::cout<<hamiltonian;
 	std::cout.precision(12);
@@ -136,10 +187,7 @@ int main(int argc, char* argv[])
 		double correlation = computeCorrelation(site, gs, bos);
 		std::cout<<site<<" "<<correlation<<"\n";
 	}
-	
+
 	std::cout<<"\n";
-
-
-
 }
 
