1c4903afeb5d1c1179ebe8413b98e1fcc16e0b3b
Gonzalo Alvarez <gz1@ornl.gov>
2025-07-23 20:27:06 -0400

    formatted

--- Freefermions/energy.cpp
+++ Freefermions/energy.cpp
@@ -1,215 +1,185 @@
-#include <iostream>
-#include <iomanip>
 #include "../../codes/PsimagLite/src/Matrix.h"
+#include <iomanip>
+#include <iostream>
 
 // states | 0 0 1 0> for N = 4
-    // c|0> = 0 and c|1> = 0; c
-    // c^\dagger |0> = |1>    c^\dagger|1> = 0
-    // c^\dagger_2| 1 0 1 0> =  -| 1 1 1 0> (fermion sign)
-    //
-    // H = \sum_ij A(i, j) c^\dagger_i c_j + H.C., but A_ii = 0 for all
-    // ind must be diffrent than jnd
-    // HW correct this for when they are equal 
-    //
-    //
+// c|0> = 0 and c|1> = 0; c
+// c^\dagger |0> = |1>    c^\dagger|1> = 0
+// c^\dagger_2| 1 0 1 0> =  -| 1 1 1 0> (fermion sign)
+//
+// H = \sum_ij A(i, j) c^\dagger_i c_j + H.C., but A_ii = 0 for all
+// ind must be diffrent than jnd
+// HW correct this for when they are equal
+//
+//
 
-double density(unsigned int j,unsigned int hilbert, PsimagLite::Matrix<double>& hamiltonian){
-
-unsigned int maskj = (1<<j);	
-
-double expect = 0;
-for (unsigned int k = 0; k < hilbert; ++k)
+double density(unsigned int j, unsigned int hilbert, PsimagLite::Matrix<double>& hamiltonian)
 {
-	
-expect += (k & maskj) ? hamiltonian(k,0)*hamiltonian(k,0) : 0;
 
-}
+	unsigned int maskj = (1 << j);
 
-return expect;
+	double expect = 0;
+	for (unsigned int k = 0; k < hilbert; ++k) {
 
-
-
-}
-
-
-double correlation(unsigned int ind, unsigned int jnd, const std::vector<double>& gs){
-	double sum = 0;
-	for (unsigned int a = 0; a < gs.size(); ++a){
-	unsigned int maskind = (1<<ind);
-	unsigned int maskjnd = (1<<jnd);
-
-
-	if (ind == jnd) {
-		sum += gs[a]*gs[a];
-		continue;
-	} else if ((maskjnd & a) == 0 || (maskind & a) ){
-		continue;
+		expect += (k & maskj) ? hamiltonian(k, 0) * hamiltonian(k, 0) : 0;
 	}
-	unsigned int mask = maskind | maskjnd;		
-	unsigned int b = a ^ mask;
 
-	sum += gs[a] * gs[b];
-	
+	return expect;
+}
 
+double correlation(unsigned int ind, unsigned int jnd, const std::vector<double>& gs)
+{
+	double sum = 0;
+	for (unsigned int a = 0; a < gs.size(); ++a) {
+		unsigned int maskind = (1 << ind);
+		unsigned int maskjnd = (1 << jnd);
 
+		if (ind == jnd) {
+			sum += gs[a] * gs[a];
+			continue;
+		} else if ((maskjnd & a) == 0 || (maskind & a)) {
+			continue;
+		}
+		unsigned int mask = maskind | maskjnd;
+		unsigned int b = a ^ mask;
+
+		sum += gs[a] * gs[b];
 	}
 
 	return sum;
-}	
-
-
+}
 
 PsimagLite::Matrix<double> correlations(unsigned int n, const std::vector<double>& gs)
 {
 
-PsimagLite::Matrix<double> corr(n,n);		
- for(unsigned int i=0; i < n; ++i)
- {
-	for(unsigned int j=0; j < n; ++j)
- {
-	corr(i,j) = correlation(i,j,gs);	
-
-
- }
- }	 
+	PsimagLite::Matrix<double> corr(n, n);
+	for (unsigned int i = 0; i < n; ++i) {
+		for (unsigned int j = 0; j < n; ++j) {
+			corr(i, j) = correlation(i, j, gs);
+		}
+	}
 	return corr;
 }
 
-double square(double x){
+double square(double x)
+{
 
-	return x*x;
-
+	return x * x;
 }
 
+void energy(unsigned int N)
+{
 
+	unsigned int hilbert = (1 << N);
+	PsimagLite::Matrix<double> hamiltonian(hilbert, hilbert);
+	unsigned int middle1 = N / 2;
+	// This is the electric field at a site
+	double v_0 = 0;
+	// A_ii = v_0 if i = middle1 or midddle2
+	// zero o.w.
+	for (unsigned int col = 0; col < hilbert; ++col) {
+		unsigned int maskmid1 = (1 << middle1);
+		hamiltonian(col, col) = (col & maskmid1) ? v_0 : 0;
 
-void energy(unsigned int N){
-
-    unsigned int hilbert = (1<<N); 
-    PsimagLite::Matrix<double> hamiltonian(hilbert, hilbert);
-    unsigned int middle1 = N/2;
-    //This is the electric field at a site
-    double v_0 = 0;
-    //A_ii = v_0 if i = middle1 or midddle2 
-    //zero o.w.
-    for (unsigned int col = 0 ;  col < hilbert; ++col) {
-	    unsigned int maskmid1 = (1<<middle1);
-	    hamiltonian(col,col) =  (col & maskmid1) ? v_0 : 0; 
-	    
-	    // H|row> = \sum_{col} factor* |col>
-	     // H(row, col) = factor
-       	     //The first and last column will always be all 0
-	     for (unsigned int i = 0; i < N; i++) {
-		unsigned int j = i + 1;
-                if(j == N)
-		{
-			continue;
-		}
-
-	     	     unsigned int maski = (1<<i);
-		     unsigned int maskj = (1<<j); // puts a 1 in location j,
-		     unsigned int mask = maski | maskj;
-		     unsigned int ri = (col & maski) ? 1 : 0; // if not zero then true 
-		     unsigned int rj = (col & maskj) ? 1 : 0;
-		     //We only want one 'hit'
-		     //putting this snip here for effecacy	
-		     if (ri == rj){
-			     continue;
+		// H|row> = \sum_{col} factor* |col>
+		// H(row, col) = factor
+		// The first and last column will always be all 0
+		for (unsigned int i = 0; i < N; i++) {
+			unsigned int j = i + 1;
+			if (j == N) {
+				continue;
 			}
 
+			unsigned int maski = (1 << i);
+			unsigned int maskj = (1 << j); // puts a 1 in location j,
+			unsigned int mask = maski | maskj;
+			unsigned int ri = (col & maski) ? 1 : 0; // if not zero then true
+			unsigned int rj = (col & maskj) ? 1 : 0;
+			// We only want one 'hit'
+			// putting this snip here for effecacy
+			if (ri == rj) {
+				continue;
+			}
 
-		     unsigned int row = col ^ mask;
-		     
-		     // fermion sign
-		     // n.particles = sites with the bit 1 ("the bit is set")
-		     // nci = n. particle before site i = \sum_{l < i} n_l
-		     // ncj = ... \sum_{l < j} n_l
-		     // factor = (-1)^{nci+ncj} = (-1) \sum_{l < i} {n_l} + \sum_{l < i} {n_l} + \sum_{i <= l < j} {n_l}}=
-		     // factor = (-1)^{2*\sum_{l < i} {n_l} + \sum_{i <= l < j} {n_l}} } = (-1)^\sum_{i <= l < j} {n_l}}
-		     // If i < j which it is in this loop
-		     // count between i and j
-		     // Maybe j does not matter here
-		    unsigned int maskbari = maski - 1;// = 2^i - 1 has 1 in all bits 00001111 for l < 1 is 1
-		    unsigned int maskbarj = maskj - 1;
-		    unsigned int maskbar = maskbari & maskbarj;
-		    unsigned int count = col & ~maskbar;
-		     //ncount is the number of bits in count ==> factor = (ncount & 1) ? -1 : 1
-		    int ncount = std::popcount(count); 
-                     hamiltonian(row, col) = (ncount &1)? 1 : -1;
-		    				     // factor = 1 or -1
-						     // We include the ones that should be zero
-						     // \daggeri      .j
-						     // 0              1
-						     // 1              0  <--- this situation is the Hermitian conjugate
-						     //
-						     // 0             0  <-- gives 0
-						     // 1             1  <-- gives 0
- 
+			unsigned int row = col ^ mask;
+
+			// fermion sign
+			// n.particles = sites with the bit 1 ("the bit is set")
+			// nci = n. particle before site i = \sum_{l < i} n_l
+			// ncj = ... \sum_{l < j} n_l
+			// factor = (-1)^{nci+ncj} = (-1) \sum_{l < i} {n_l} + \sum_{l < i} {n_l} + \sum_{i <= l < j} {n_l}}=
+			// factor = (-1)^{2*\sum_{l < i} {n_l} + \sum_{i <= l < j} {n_l}} } = (-1)^\sum_{i <= l < j} {n_l}}
+			// If i < j which it is in this loop
+			// count between i and j
+			// Maybe j does not matter here
+			unsigned int maskbari = maski - 1; // = 2^i - 1 has 1 in all bits 00001111 for l < 1 is 1
+			unsigned int maskbarj = maskj - 1;
+			unsigned int maskbar = maskbari & maskbarj;
+			unsigned int count = col & ~maskbar;
+			// ncount is the number of bits in count ==> factor = (ncount & 1) ? -1 : 1
+			int ncount = std::popcount(count);
+			hamiltonian(row, col) = (ncount & 1) ? 1 : -1;
+			// factor = 1 or -1
+			// We include the ones that should be zero
+			// \daggeri      .j
+			// 0              1
+			// 1              0  <--- this situation is the Hermitian conjugate
+			//
+			// 0             0  <-- gives 0
+			// 1             1  <-- gives 0
+		}
 	}
 
+	// Print results
+	// std::cout<<hamiltonian;
+	std::vector<double> eigs(hilbert);
 
-}
+	diag(hamiltonian, eigs, 'V');
+	double energyin = eigs[0] / N;
 
-//Print results 
-//std::cout<<hamiltonian;
-std::vector<double> eigs(hilbert);
+	std::cout << energyin << "\n";
 
+	/*std::vector<double> gs(hilbert);
+	for (unsigned int k = 0; k < hilbert; ++k  )
+	{
 
-diag(hamiltonian, eigs, 'V');
-double energyin = eigs[0]/N;
+		gs[k] = hamiltonian(k,0);
 
-std::cout<<energyin<<"\n";
+	}
+	*/
+	// <gs|c^dag_1 c_1| gs>
 
-/*std::vector<double> gs(hilbert);
-for (unsigned int k = 0; k < hilbert; ++k  )
-{
+	// std::cout<<gs<<"\n";
+	// PsimagLite::Matrix<double> corr = correlations(N,gs);
+	// std::cout<<corr;
 
-	gs[k] = hamiltonian(k,0);
+	/*
+	std::vector<double> dens(N);
+	for (unsigned int j = 0; j < N; ++j)
+	{
 
-}
-*/
-// <gs|c^dag_1 c_1| gs>
-
-//std::cout<<gs<<"\n";
-//PsimagLite::Matrix<double> corr = correlations(N,gs);
-//std::cout<<corr;
-
-/*
-std::vector<double> dens(N);
-for (unsigned int j = 0; j < N; ++j)
-{
-
-dens[j] = density(j,hilbert, hamiltonian);
+	dens[j] = density(j,hilbert, hamiltonian);
 
 
-}
-*/
-
+	}
+	*/
 }
 
 int main(int argc, char* argv[])
 {
 
-	//name n:number of sites m: 0 for even 1 for odd
+	// name n:number of sites m: 0 for even 1 for odd
 
-	if (argc < 2){
+	if (argc < 2) {
 
-	std::cerr << "Usage:" << argv[0] << " <sites>\n";
-	return 1;
-
+		std::cerr << "Usage:" << argv[0] << " <sites>\n";
+		return 1;
 	}
 
 	unsigned int n = std::atoi(argv[1]);
 	// the initialzation doesn't look great, but I want to keep 1 odd
-	for (unsigned int i = 4 - n; i < 11; i +=2){
+	for (unsigned int i = 4 - n; i < 11; i += 2) {
 
-	energy(i);
-
+		energy(i);
 	}
-
-
 }
-
-
-
-
--- ising/ising.cpp
+++ ising/ising.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
-#include <iomanip>
 #include "PsimagLite/src/Matrix.h"
+#include <iomanip>
+#include <iostream>
 
 extern "C" {
-    void dgeev_(char* jobvl, char* jobvr, int* n, double* a, int* lda,
-                double* wr, double* wi, double* vl, int* ldvl,
-                double* vr, int* ldvr, double* work, int* lwork, int* info);
+void dgeev_(char* jobvl, char* jobvr, int* n, double* a, int* lda, double* wr, double* wi, double* vl, int* ldvl, double* vr, int* ldvr, double* work, int* lwork, int* info);
 }
 
 // <gs|sigmax_0 sigmax_site |gs>
@@ -15,10 +13,11 @@ double computeCorrelation(unsigned int site, const std::vector<double>& gs)
 	double sum = 0;
 	for (unsigned int alpha = 0; alpha < hilbert; ++alpha) {
 		// This masks two sites: site site, and site 0
-		unsigned mask = (1<<site) | 1;
-		if (site == 0) mask = 0;
+		unsigned mask = (1 << site) | 1;
+		if (site == 0)
+			mask = 0;
 		unsigned beta = alpha ^ mask;
-		sum += gs[alpha]*gs[beta];
+		sum += gs[alpha] * gs[beta];
 	}
 
 	return sum;
@@ -27,7 +26,7 @@ double computeCorrelation(unsigned int site, const std::vector<double>& gs)
 int main(int argc, char* argv[])
 {
 	if (argc != 4) {
-		std::cerr<<"USAGE: "<<argv[0]<<" number_of_sites J periodic\n";
+		std::cerr << "USAGE: " << argv[0] << " number_of_sites J periodic\n";
 		return 1;
 	}
 
@@ -41,22 +40,22 @@ int main(int argc, char* argv[])
 	// Example: n = 3, example basis state |alice> = |0> | 1> |1>
 	// Norm squared of the basis states is 1: <0|0> = 1, <1|1> = 1
 	//
-	// H = J*\sum_{i, i + 1} \sigmax_i \otimes \sigmax_{i + 1} 
+	// H = J*\sum_{i, i + 1} \sigmax_i \otimes \sigmax_{i + 1}
 	// Example n = 3        ---> H = sigmax_0 sigmax_1 + sigmax_1 sigmax_2 + sigmax_2 sigmax_0
 	// There are a identities implied!
 
 	// sigmax acts only on one site: sigmax |0> = |1> and sigmax|1> = |0>
-	// 
+	//
 	// Example H |alice> = |1> | 0> |1> + | 0> |0> |0> + |1> | 1> |0>
 	//
 	// Fill the matrix H for an Ising model
-	unsigned int hilbert = (1<<n); //2^n;
- 	PsimagLite::Matrix<double> hamiltonian(hilbert, hilbert);	
+	unsigned int hilbert = (1 << n); // 2^n;
+	PsimagLite::Matrix<double> hamiltonian(hilbert, hilbert);
 
 	// More intuitive way maybe:
 	// loop over row and nested loop over col <col|H|row> = complex number = H(row, col)
 	//
-	
+
 	for (unsigned int row = 0; row < hilbert; ++row) {
 		// Every integer from 0 to 2^n - 1 represents a state of the (computational) basis
 		// Example Alice looks like 0 1 1 --> interpret it as a binary number --> 3
@@ -65,7 +64,7 @@ int main(int argc, char* argv[])
 		// H|3> = [1, 0, 0, 0, 0, 1, 1, 0]
 		//
 		for (unsigned int site = 0; site < n; ++site) {
-			// Starting with state row 
+			// Starting with state row
 			// flip bit number site and bit number site + 1
 			// We need to worry about border, like if site + 1 >= n
 			// row =      bit(n-1) bit(n-2) ... bit(site+1) bit(site) ... bit(1) bit(0)
@@ -84,13 +83,13 @@ int main(int argc, char* argv[])
 
 			unsigned int next_site = (site + 1 == n) ? 0 : site + 1;
 
-			unsigned int mask = (1<<site) | (1<<next_site); // The "|" or binary op
+			unsigned int mask = (1 << site) | (1 << next_site); // The "|" or binary op
 			unsigned int col = row ^ mask; // ^ is the xor binary op
 			hamiltonian(row, col) = couplingJ;
 		}
 	}
 
-	//std::cout<<hamiltonian;
+	// std::cout<<hamiltonian;
 
 	// Diagonalize it and find eigenvalues --> homework
 	// Think about the ground state, what energy does it have?
@@ -98,7 +97,8 @@ int main(int argc, char* argv[])
 	std::vector<double> eigs(hilbert);
 
 	diag(hamiltonian, eigs, 'V');
-	std::cout<<eigs[0]<<"\n";;
+	std::cout << eigs[0] << "\n";
+	;
 	std::vector<double> gs(hilbert);
 	for (unsigned int k = 0; k < hilbert; ++k) {
 		gs[k] = hamiltonian(k, 0);
@@ -106,12 +106,8 @@ int main(int argc, char* argv[])
 
 	for (unsigned int site = 0; site < n; ++site) {
 		double correlation = computeCorrelation(site, gs);
-		std::cout<<site<<" "<<correlation<<"\n";
+		std::cout << site << " " << correlation << "\n";
 	}
-	
-	std::cout<<"\n";
 
-
-
+	std::cout << "\n";
 }
-
